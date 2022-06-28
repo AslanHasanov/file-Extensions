30c4ad12a0c79a394f0a349a8b1304dc7f101c32
AslanHasanov <aslanrh@code.edu.az>
2022-06-28 19:11:37 +0400

    Added this constructor and methods

--- File Extensions/File Extensions/Program.cs
+++ File Extensions/File Extensions/Program.cs
@@ -32,20 +32,53 @@ namespace File_Extensions
         private string[] _fileExtensions;
         private string _fileName;
 
-
-
-
         public FileValidator(params string[] extensions)
-
         {
             _fileExtensions = extensions;
-
         }
-
-        public FileValidator(string fileName, params string[] extensions)
+        public FileValidator(string fileName, params string[] extensions) :this(extensions)
         {
             _fileName = fileName;
-            _fileExtensions = extensions;
+        }
+
+        public bool IsExtensionTrue(string name)
+        {
+            string newExtension = "";
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == '.')
+                {
+                    for (int a = i; a < name.Length; a++)
+                    {
+                        newExtension += name[a];
+                    }
+                }
+            }
+            foreach (string sonluq in _fileExtensions)
+            {
+                if (sonluq == newExtension)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string DefineExtension(string name)
+        {
+            string fullName = _fileName;
+            string targetFileName = "";
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (fullName[i] == '.')
+                {
+                    for (int o = i; o > fullName.Length; o--)
+                    {
+                        targetFileName += fullName[o];
+                    }
+                }
+            }
+            return fullName;
         }
     }
 }
